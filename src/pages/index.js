54de333d7ee2afeaf140b6740a678c62596acabc
Ion <19718355+Nei-V@users.noreply.github.com>
2018-10-05 19:36:36 +0300

    updated first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,11 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>JAMstack freeCodeCamp Online Hackaton Project</h1>
+    <p>I'm Ion, I'm learning web development and I'll love to take part in my first Hackaton.</p>
+    <p>You can visit my GitHub repository here:</p>
+<a href="https://github.com/Nei-V" target="blank">Ion Varsescu's Github Repositories</a>    
+    <Link to="/page-2/">More about me</Link>
   </Layout>
 )
 
