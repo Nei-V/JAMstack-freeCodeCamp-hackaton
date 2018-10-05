32a3ae0617a435c4cbc38f3b061a9bb0781ba252
Ion <19718355+Nei-V@users.noreply.github.com>
2018-10-05 19:52:45 +0300

    updated start-up pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,10 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>JAMstack freeCodeCamp Online Hackaton Project</h1>
-    <p>I'm Ion, I'm learning web development and I'll love to take part in my first Hackaton.</p>
+    <p>I'm Ion, I'm learning web development and I'll love to take part in my first hackaton.</p>
     <p>You can visit my GitHub repository here:</p>
-<a href="https://github.com/Nei-V" target="blank">Ion Varsescu's Github Repositories</a>    
+<a href="https://github.com/Nei-V" target="blank">Ion Varsescu's Github Repositories</a> 
+<br />   
     <Link to="/page-2/">More about me</Link>
   </Layout>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,8 +5,18 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>You can learn more about me and my activity by following these links</h1>
+    <br />
+    <br />
+    <a href="https://www.linkedin.com/in/ionvarsescu/" target="blank">Ion Varsescu on Linkedin</a> 
+    <br />
+    <a href="https://www.facebook.com/varsescu.ion" target="blank">Ion Varsescu on Facebook</a> 
+    <br />
+    <a href="https://www.facebook.com/groups/free.code.camp.haifa/" target="blank">FreeCodeCamp Haifa Google Group</a> 
+    <br />
+    <a href="http://ionv.co.nf/" target="blank">Ion Varsescu's Personal Blog</a> 
+    <br />
+    <br />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
